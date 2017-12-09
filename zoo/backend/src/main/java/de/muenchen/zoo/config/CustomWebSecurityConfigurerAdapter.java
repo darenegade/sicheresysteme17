c4ebe92ef5b89a65aca57aecaaa9281831097e4c
René Zarwel <darenegade@gmx.de>
2017-12-09 20:01:03 +0100

    Resolve App3.1.A1

---
+++ zoo/backend/src/main/java/de/muenchen/zoo/config/CustomWebSecurityConfigurerAdapter.java
@@ -0,0 +1,41 @@
+package de.muenchen.zoo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Organization: HM FK07.
+ * Project: zoo, de.muenchen.zoo.config
+ * Author(s): Rene Zarwel
+ * Date: 09.12.17
+ * OS: MacOS 10.11
+ * Java-Version: 1.8
+ * System: 2,3 GHz Intel Core i7, 16 GB 1600 MHz DDR3
+ */
+@Configuration
+@EnableWebSecurity
+public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("leiter").password("1234")
+                .authorities("ROLE_LEITER")
+                .and()
+                .withUser("pfleger").password("4321")
+                .authorities("ROLE_PFLEGER");
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}
---
+++ zoo/backend/src/main/java/de/muenchen/zoo/config/TomcatServletConfig.java
@@ -0,0 +1,52 @@
+package de.muenchen.zoo.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Organization: HM FK07.
+ * Project: zoo, de.muenchen.zoo.config
+ * Author(s): Rene Zarwel
+ * Date: 09.12.17
+ * OS: MacOS 10.11
+ * Java-Version: 1.8
+ * System: 2,3 GHz Intel Core i7, 16 GB 1600 MHz DDR3
+ */
+@Configuration
+public class TomcatServletConfig {
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+        return tomcat;
+    }
+
+    private Connector initiateHttpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+
+        return connector;
+    }
+
+}
